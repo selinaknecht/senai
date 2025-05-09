5e4d6eaec8e4865ab682ec36d7092ef650f54d44
selinaknecht <selina_knecht@estudante.sesisenai.org.br>
2025-05-09 22:27:47 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-mani
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/calculadora", methods=["GET", "POST"])
+def calculadora():
+    resultado = None
+    if request.method == "POST":
+        try:
+            num1 = float(request.form["num1"])
+            num2 = float(request.form["num2"])
+            operacao = request.form["operacao"]
+
+            if operacao == "soma":
+                resultado = num1 + num2
+            elif operacao == "subtracao":
+                resultado = num1 - num2
+            elif operacao == "multiplicacao":
+                resultado = num1 * num2
+            elif operacao == "divisao":
+                if num2 != 0:
+                    resultado = num1 / num2
+                else:
+                    resultado = "Erro: Divisão por zero"
+        except:
+            resultado = "Erro nos dados fornecidos"
+
+    return render_template("calculadora.html", resultado=resultado)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
